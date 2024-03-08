253c81a17441be9be759ac65c32eb022981896ad
Bratoun <rayan.ibrahime@iut-rodez.fr>
2024-03-08 14:44:52 +0100

    Update StockModele.php

--- dolibi/modeles/StockModele.php
+++ dolibi/modeles/StockModele.php
@@ -7,8 +7,11 @@ class StockModele
 {
 
     function listeFournisseur($url,$apikey) {
+		$urlThirdParties = http://dolibarr.iut-rodez.fr/G2023-42/htdocs/api/index.php/thirdparties?fields=id,type&sqlfilters=type=%271%27
 		$urlConnexion = $url."api/index.php/supplierinvoices?sortfield=t.rowid&sortorder=ASC&limit=100";
-		$apiKey = BanqueModele::appelAPI($urlConnexion,$apikey,$iut);
+
+		// 
+		$apiKey = UserModele::appelAPI($urlConnexion,$apikey,$iut);
 		return $apiKey;
     }
 }
