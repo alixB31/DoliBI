90db5bac62f822acd4cc3aef12f9e8b523e55e98
Bratoun <rayan.ibrahime@iut-rodez.fr>
2024-03-08 14:49:57 +0100

    commentaire sqlfilters

--- DoliBI/modeles/StockModele.php
+++ DoliBI/modeles/StockModele.php
@@ -7,6 +7,7 @@ class StockModele
 {
 
     function listeFournisseur($url,$apikey) {
+		$urlThirdParties = http://dolibarr.iut-rodez.fr/G2023-42/htdocs/api/index.php/thirdparties?fields=id,type&sqlfilters=type=%271%27
 		$urlConnexion = $url."api/index.php/supplierinvoices?sortfield=t.rowid&sortorder=ASC&limit=100";
 		$apiKey = BanqueModele::appelAPI($urlConnexion,$apikey,$iut);
 		return $apiKey;
--- dolibi/modeles/StockModele.php
+++ dolibi/modeles/StockModele.php
@@ -9,9 +9,7 @@ class StockModele
     function listeFournisseur($url,$apikey) {
 		$urlThirdParties = http://dolibarr.iut-rodez.fr/G2023-42/htdocs/api/index.php/thirdparties?fields=id,type&sqlfilters=type=%271%27
 		$urlConnexion = $url."api/index.php/supplierinvoices?sortfield=t.rowid&sortorder=ASC&limit=100";
-
-		// 
-		$apiKey = UserModele::appelAPI($urlConnexion,$apikey,$iut);
+		$apiKey = BanqueModele::appelAPI($urlConnexion,$apikey,$iut);
 		return $apiKey;
     }
 }
